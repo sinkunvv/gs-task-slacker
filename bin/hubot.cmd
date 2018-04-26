8d731ef66c2a1d006e7251f9bed6a64e847754e5
sinkunvv <sinkunvv@gmail.com>
2018-04-26 02:30:43 +0000

    hubot name fixed

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -4,4 +4,4 @@ call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
-node_modules\.bin\hubot.cmd --name "CCDタスク管理" %* 
+node_modules\.bin\hubot.cmd --name "taslack" %* 
